3af3cb3162e8cebf45d7b35e9f0fb176c35212c7
Sadat Alam Protik <protik0939@gmail.com>
2024-09-24 22:06:21 +0600

    admin link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://alphaoneedu.com'];
+const allowedOrigins = ['https://sso.alphaoneedu.com', 'https://alphaoneedu.com', 'http://localhost:5173', 'http://localhost:5174'];
 
 // Define a CORS options delegate function
 const corsOptionsDelegate = function (req, callback) {
